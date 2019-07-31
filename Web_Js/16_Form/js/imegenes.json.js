219510de3a78862ed6f50ba9c306f89bb97cc19c
Sara <CursosTardes@tic.loc>
2019-07-31 16:14:44 +0200

    Clon profe: class y 16_Form

---
+++ Web_Js/16_Form/js/imegenes.json.js
@@ -0,0 +1,12 @@
+    
+ export default 
+ [
+    {"nombre": "Pino",
+    "source": "pino.jpg"},
+    {"nombre": "Abeto",
+    "source": "Abeto.jpg"},
+    {"nombre": "Higuera",
+    "source": "higuera.jpg"},
+    {"nombre": "Melocotonero",
+    "source": "melocotonero.jpg"}  
+]
