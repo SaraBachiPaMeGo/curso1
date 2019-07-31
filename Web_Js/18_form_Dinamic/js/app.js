66c4298c1ac4b8dd9b7fcf0b11c6c86e3863e418
Sara <CursosTardes@tic.loc>
2019-07-31 20:59:19 +0200

    Ejercicio dinámico profe

--- Web_Js/18_form_Dinamic/js/app.js
+++ Web_Js/18_form_Dinamic/js/app.js
@@ -1,116 +1,46 @@
-import { validacion, validacionFinal } from "./validacion.js";
-
 export function app() {
     console.log('Cargada app') 
 
-    // Variables
+    const aCursosFront = [
+        {code: "HTML-01", titulo: 'HTML5'},
+        {code: "CSS-02", titulo: 'CSS3'},
+        {code: "JS-03", titulo: 'JavaScript'}
+    ]
 
-    let oDatos =  {
-        userName: '',
-        userPasswd: '',
-        email: '',
-        dni: '',
-        cv: '',
-        birthDate: '',
-        info: '',
-        isOk: 'on',
-        curso: '',
-        sitio: '',    
-    }
+    const aCursosBack = [
+        {code: "Node-04", titulo: 'Node JS'},
+        {code: "PHP-05", titulo: 'PHP'}
+    ]
 
-    let oDatosPretty =  {
-        userName: 'Nombre de Usuario',
-        userPasswd: 'Contraseña',
-        email: 'Correo electrónico',
-        dni: 'D.N.I.',
-        cv: 'Formación',
-        birthDate: 'Fecha de nacimiento',
-        info: 'Más información',
-        isOk: 'Condiciones aceptadas',
-        curso: 'Curso',
-        sitio: 'Sitio',    
-    }
+    const aSelects = document.querySelectorAll('select')
+    aSelects[0].addEventListener('change', onSelectTech)
+    aSelects[1].addEventListener('change', onSelectCurso)
 
-    // Nodos del DOM
-    let form = document.querySelector('[name="complete-post"]')
-    let btnReseet = document.querySelector('#btnReset')
-    let aFormData = document.querySelectorAll('.form_data')
-    let aCheckBox = document.querySelectorAll('[type="checkbox"]')
-    let aSelects = document.querySelectorAll('select')
-    let aRadioSitio = document.querySelectorAll('[name="sitio"]')
-    let dlgConfirm = document.querySelector('#dlg-confirm')
-    let aDlgButtons = document.querySelectorAll('#dlg-confirm button')
 
-    // Definir manejadores
-    form.addEventListener('submit', onSubmit)
-    btnReseet.addEventListener('click', onReset)
-    aDlgButtons.forEach(btn => btn.addEventListener('click', onClickDlg))
-
-    let aNodosValidables = nodosValidables()
-    validacion()
-
-    // Funciones manejadoras de eventos
-    
-    function onSubmit(ev) {
-        ev.preventDefault()
-        console.log(`Iniciando submit`)
-
-        if (!validacionFinal(aNodosValidables)) {
-            return
+    function onSelectTech(ev) {
+        let aCursos
+        switch (ev.target.value) {
+            case 'F':
+                aCursos = aCursosFront
+                break;
+            case 'B':
+                aCursos = aCursosBack
+                break;
         }
-        aFormData.forEach(item => oDatos[item.name] = item.value)
-        aCheckBox.forEach(item => oDatos[item.name] = item.checked)
-        aSelects.forEach(item => oDatos[item.name] = item.value)
-        oDatos.sitio = getRadio(aRadioSitio)
-        renderModal()
+        let html = '<option value="">Elige un curso</option>'
+        aCursos.forEach(item => {
+            html += `
+            <option value="${item.code}">${item.titulo}
+            </option>
+            `
+        })
+        aSelects[1].innerHTML = html
+        aSelects[1].disabled = false 
     }
 
-    function nodosValidables() {
-        let aNodos = []
-        aFormData.forEach(
-            item => aNodos.push(item)
-        )
-        aCheckBox.forEach(
-            item => aNodos.push(item)
-        )
-        aSelects.forEach(
-            item => aNodos.push(item)
-        )
-        aNodos.push(aRadioSitio[0])
-        return aNodos.filter(item => !item.checkValidity())
+
+    function onSelectCurso(ev) {
+        console.log('Elegido el curso', ev.target.value )
+
     }
-
-    function onReset(ev) {}
-
-    function onClickDlg(ev) {
-        if(ev.target.textContent.toLowerCase() === 'si') {
-            form.submit()
-        }
-        dlgConfirm.close() 
-    }
-
-    function renderModal() {
-        let html = ''
-        for (const key in oDatos) {
-                const value = oDatos[key];
-                if(key === 'userPasswd') {
-                    html += `<li>${oDatosPretty[key]} --> *****</li>`
-                } else if (key === 'isOk') {
-                    html += `<li>${oDatosPretty[key]}</li>`
-                } else {
-                    html += `<li>${oDatosPretty[key]} --> ${value}</li>`    
-                }
-        }
-        dlgConfirm.children[1].innerHTML = html
-        dlgConfirm.showModal()
-    }
-
-    function getRadio(aNodos) {
-        for (let i = 0; i < aNodos.length; i++) {
-            const item = aNodos[i];
-            if(item.checked) {
-                return item.value}
-        }
-    }
-
  }
