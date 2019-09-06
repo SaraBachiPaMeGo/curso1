3548a30ba7fe963c658ab827a606bf223f741804
Sara <CursosTardes@tic.loc>
2019-09-06 19:33:52 +0200

    Procesando css

--- Web_Js/24_EjercicioPaises/js/app.js
+++ Web_Js/24_EjercicioPaises/js/app.js
@@ -6,59 +6,102 @@ export function app() {
     console.log('App Cargada')
 
     let aName = []
+    let actualPais = {}
+    let newUrl = url + '/'
+    let nombrePais = ''
 
     //Nodos del DOM 
     let slcContinent = document.querySelector('#slc-continent')
     let newSlc = document.querySelector('#new-slc') // Haremos el innerHTML con otro select
+    let newDiv = document.querySelector('#new-div')
     let aSlcCont = {
         europa: document.querySelector('#europa'),
         africa: document.querySelector('#africa'),
         asia: document.querySelector('#asia'),
         america: document.querySelector('#america'),
         oceania: document.querySelector('#oceania'),
-    }
-    let slcCountry = document.querySelector('#slc-country') // innerHTML
+    }    
 
     //Asociación de eventos
     slcContinent.addEventListener('change', conection)
 
-    /* slcContinent.addEventListener('click',onClick)
-    slcContinent.addEventListener('click',onClick) */ /* SLC-CONTINENT.VALUE --> OPTION */
-
     //Funciones manejadoras
 
     function conection(ev) { // Hacemos la conexión con la BD y obtenemos los datos
-        let newUrl = url + '/' + ev.target.value /* Hacer una url que va a ser igual a la que tenemos más el value del option */
-        /* console.log(ev.target.value) */
+        console.log(ev.target.value)
+        newUrl =  newUrl+ ev.target.value /* Hacer una url que va a ser igual a la que tenemos más el value del option */
         fetch(newUrl)
             .then(response => response.json())
             .then(data => {
-                data = data.map(nombre => {
-                    return { pepe: nombre.name }
-                    
+                aName = data.map(nombre => {
+                    return { nombre: nombre.name }                    
                 })
-                console.log(data)
+                renderDatos()
+                
             })
+    }
 
+    function conection2(ev) { 
+        console.log(ev.target.value + 'ev.target connection2') //Sale europa
+        
+        let url = 'https://restcountries.eu/rest/v2/name/'+ ev.target.value + '?fullText=true'//Necesito el nombre del país
+        /* newUrl = ev.target.value  + 's=name;capital;flag;currencies;languages' */
+        
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                actualPais = data.map(info => {
+                    return { 
+                        capital: info.capital,
+                        flag: info.flag,
+                        languages: info.languages.map(idioma=>{
+                            return  idioma.name
+                        })                       
+                    }                    
+                })
+                renderDatos2()                
+            })
+    }
 
+    function newNodo() {
+        let slcCountry = document.querySelector('#slc-country') // innerHTML
+        slcCountry.addEventListener('change', conection2)
     }
 
     //Otras funciones
 
     function renderDatos(ev) { //Nos muestra los países       
+        
+        let html = '<select name="select2" id="slc-country">'
 
-        let html = ''
-
-        aDatos.forEach(pais => {
+        aName.forEach(pais => {
             html += `
-            <select name="select2" id="slc-country">
-            <option value="europa" id="opt-pais">${pais.name}</option>
+            <option value="${pais.nombre}" id="opt-pais">${pais.nombre}</option>
             `
+            actualPais = pais
+            
         });
 
         newSlc.innerHTML = html
+        newNodo()
     }
 
+    function renderDatos2(ev) { //Nos muestra los países       
+        
+        let html = ' <div>'
+
+        actualPais.forEach(pais => {
+            html += `
+            <span>CAPITAL</span>  <p>${pais.capital}</p>
+            <span>BANDERA </span><p>${pais.flag}</p>
+            <img src="${pais.flag}" alt="Banderas de paises">
+            <span>IDIOMA </span> <p>${pais.languages}</p>
+            </div>
+            `
+        });
+
+        newDiv.innerHTML = html
+    }
 }
 
 
