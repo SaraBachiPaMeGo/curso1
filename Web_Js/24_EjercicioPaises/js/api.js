657c240ac332a6bb8ec967078f463db4a2578b90
Sara Bachiller <sarabachiller8@gmail.com>
2019-09-09 14:16:04 +0200

    a

--- Web_Js/24_EjercicioPaises/js/api.js
+++ Web_Js/24_EjercicioPaises/js/api.js
@@ -1,13 +1,13 @@
 export {url}
-export {aDatos}
-export {resultadoFilter}
+export {aName}
+export {resultado}
 
 let url = 'https://restcountries.eu/rest/v2/region'
-let aDatos = []
-let resultadoFilter = []
+let aName = []
+let resultado = []
 
 
-
+{
 /* {"name":"Afghanistan",
 "topLevelDomain":[".af"],
 "alpha2Code":"AF",
@@ -20,8 +20,26 @@ let resultadoFilter = []
 "region":"Asia",
 "subregion":"Southern Asia",
 "population":27657145,
-"latlng":[33.0,65.0],"demonym":"Afghan","area":652230.0,"gini":27.8,"timezones":["UTC+04:30"],"borders":["IRN","PAK","TKM","UZB","TJK","CHN"],"nativeName":"افغانستان","numericCode":"004","currencies":[{"code":"AFN","name":"Afghan afghani","symbol":"؋"}],"languages":[{"iso639_1":"ps","iso639_2":"pus","name":"Pashto","nativeName":"پښتو"} */
-
+"latlng":[33.0,65.0],
+"demonym":"Afghan",
+"area":652230.0,
+"gini":27.8,
+"timezones":["UTC+04:30"],
+"borders":["IRN","PAK","TKM","UZB","TJK","CHN"],
+"nativeName":"افغانستان",
+"numericCode":"004",
+"currencies":[
+    {"code":"AFN",
+    "name":"Afghan afghani",
+    "symbol":"؋"}
+],
+"languages":[
+    {"iso639_1":"ps",
+    "iso639_2":"pus",
+    "name":"Pashto",
+    "nativeName":"پښتو"} 
+]} */
+}
 
 /* Todos los paises cuy region sea el value del slcContinent.value */
 
--- Web_Js/24_EjercicioPaises/js/app.js
+++ Web_Js/24_EjercicioPaises/js/app.js
@@ -1,15 +1,18 @@
 import { url } from './api.js'
-import { aDatos } from './api.js'
-import { resultadoFilter } from './api.js'
+import { aName } from './api.js'
+import { resultado } from './api.js'
 
 export function app() {
     console.log('App Cargada')
-
-    let aName = []
+    let aName = [] /* CUANDO LA VARIABLE ESTÁ EN LA API ME DA UN ERROR PORQUE LA PONE COMO CONST */
+    let aPais = []
+    let newUrl = ''
+    let actualPais =''
 
     //Nodos del DOM 
     let slcContinent = document.querySelector('#slc-continent')
     let newSlc = document.querySelector('#new-slc') // Haremos el innerHTML con otro select
+    let newDiv = document.querySelector('#new-div')
     let aSlcCont = {
         europa: document.querySelector('#europa'),
         africa: document.querySelector('#africa'),
@@ -17,46 +20,83 @@ export function app() {
         america: document.querySelector('#america'),
         oceania: document.querySelector('#oceania'),
     }
-    let slcCountry = document.querySelector('#slc-country') // innerHTML
-
+    
     //Asociación de eventos
     slcContinent.addEventListener('change', conection)
-
-    /* slcContinent.addEventListener('click',onClick)
-    slcContinent.addEventListener('click',onClick) */ /* SLC-CONTINENT.VALUE --> OPTION */
-
+    
     //Funciones manejadoras
 
     function conection(ev) { // Hacemos la conexión con la BD y obtenemos los datos
-        let newUrl = url + '/' + ev.target.value /* Hacer una url que va a ser igual a la que tenemos más el value del option */
+        newUrl = url + '/' + ev.target.value /* Hacer una url que va a ser igual a la que tenemos más el value del option */
         /* console.log(ev.target.value) */
         fetch(newUrl)
             .then(response => response.json())
             .then(data => {
-                data = data.map(nombre => {
-                    return { pepe: nombre.name }
-                    
+                aName = data.map(nombre => {
+                    return {
+                        nombre: nombre.name
+                    }
                 })
-                console.log(data)
+                renderDatos()
+               // console.log(aName) //Array de objetos 
             })
+    }
 
+    function conection2(ev) { 
+        newUrl = '/' + ev.target.value 
+        console.log(ev.target.value) 
+        fetch(newUrl)
+            .then(response => response.json())
+            .then(data => {
+                actualPais = data.map(info => {
+                    return {
+                        bandera: info.flag,
+                        capital: info.capital,
+                        idioma: info.languages[3].nativeName//new
+                    }
+                })
+                renderDatos2()
+                console.log(actualPais) 
+            })
+    }
 
+    function newNodos() {
+        let slcCountry = document.querySelector('#slc-country') // innerHTML
+        slcCountry.addEventListener('change', conection2)
     }
 
     //Otras funciones
 
-    function renderDatos(ev) { //Nos muestra los países       
+    function renderDatos(ev) { //Nos muestra los países
 
-        let html = ''
+        let html = `<select name="select2" id="slc-country">`
 
-        aDatos.forEach(pais => {
+        aName.forEach(pais => {
             html += `
-            <select name="select2" id="slc-country">
-            <option value="europa" id="opt-pais">${pais.name}</option>
+            <option value="lang" id="opt-pais">${pais.nombre}</option>
             `
+            actualPais = pais.nombre
+            console.log(pais)/* ${ev.target.value}<option value="europa" id="opt-pais">${aName}</option> */
         });
 
         newSlc.innerHTML = html
+        newNodos()
+    }
+
+    function renderDatos2(ev) { //Nos muestra los países  
+        let html = ``
+
+        actualPais.forEach(pais => {
+            html += `
+            <div value="opt-pais" id="opt-pais">${pais.bandera}</div>
+            <div value="opt-pais" id="opt-pais">${pais.capital}</div>
+            <div value="opt-pais" id="opt-pais">${pais.bandera}</div>
+            `
+            resultado = pais
+            console.log(resultado)/* ${ev.target.value}<option value="europa" id="opt-pais">${aName}</option> */
+        });
+
+        newDiv.innerHTML = html
     }
 
 }
